890a866940b94902425f77f43a07c6424063c24a
vinodSupnekarAtDAZN <vinod.supnekar@dazn.com>
2024-10-17 10:21:34 +0530

    interface to fetch news

---
+++ ShortNewsApp/News Feature/NewsFeed.swift
@@ -0,0 +1,13 @@
+//
+//  NewsFeed.swift
+//  ShortNewsApp
+//
+//  Created by Vinod Supnekar on 17/10/24.
+//
+
+struct NewsFeed {
+    let source: String
+    let title: String
+    let desctiption: String
+    let punlishedDate: Date
+}
