3432cdd81f83e846f8e3d987c3c7607e21a802ca
vinodSupnekarAtDAZN <vinod.supnekar@dazn.com>
2024-10-17 07:01:32 +0530

    Initial Commit

---
+++ ShortNewsApp/ShortNewsApp.h
@@ -0,0 +1,18 @@
+//
+//  ShortNewsApp.h
+//  ShortNewsApp
+//
+//  Created by Vinod Supnekar on 17/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ShortNewsApp.
+FOUNDATION_EXPORT double ShortNewsAppVersionNumber;
+
+//! Project version string for ShortNewsApp.
+FOUNDATION_EXPORT const unsigned char ShortNewsAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShortNewsApp/PublicHeader.h>
+
+
---
+++ ShortNewsAppTests/ShortNewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShortNewsAppTests.swift
+//  ShortNewsAppTests
+//
+//  Created by Vinod Supnekar on 17/10/24.
+//
+
+import Testing
+@testable import ShortNewsApp
+
+struct ShortNewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
